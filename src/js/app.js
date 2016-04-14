2e1d423dcda53cbc526476737153a5f1564702bf
yangchaoben <842293890@qq.com>
2016-04-14 10:47:04 +0800

    打赏动画完成

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+console.log('it has been running')
+
+
+function newPay(){
+	/*animation*/
+	$(".cloudy").addClass("active")
+	$(".cloudy2").addClass("active")
+	$(".cloudy3").addClass("active")
+	$(".cloudy4").addClass("active")
+	$(".cloudy5").addClass("active")
+	$(".cloudy6").addClass("active")
+	$(".cloudy7").addClass("active")
+	$(".cloudy8").addClass("active")
+	$(".pay-main").addClass("active")
+	$(".pay-main2").addClass("active")
+	$(".user-header-img").addClass("active")
+}
+//newPay()
